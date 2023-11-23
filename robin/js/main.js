7f4d4ee65b6c3183a677c506e66eb2c30c0b110f
Ivan Gaydarzhi <112941873+vinny290@users.noreply.github.com>
2023-11-23 16:41:13 +0300

    Add files via upload

--- robin/js/main.js
+++ robin/js/main.js
@@ -1,94 +1,26 @@
-var buttons = document.getElementsByClassName("button");
-var photos = document.getElementsByClassName("photo");
+document.addEventListener('DOMContentLoaded', function () {
+  const carousel = document.querySelector('.carousel')
+  const arrowLeft = document.querySelector('.arrow-left')
+  const arrowRight = document.querySelector('.arrow-right')
 
-function showPhotos(index) {
-  // Скрыть все фотографии
-  for (var i = 0; i < photos.length; i++) {
-    photos[i].style.display = "none";
-  }
-  
-  // Удалить класс "active" у всех кнопок
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("active");
-  }
-  
-  // Показать фотографии выбранной страны
-  var countryClass = "country" + (index + 1);
-  var countryPhotos = document.getElementsByClassName(countryClass);
-  for (var i = 0; i < countryPhotos.length; i++) {
-    countryPhotos[i].style.display = "block";
-  }
-  
-  // Добавить класс "active" выбранной кнопке
-  buttons[index].classList.add("active");
-}
+  let currentIndex = 0
 
-// Показать фотографии первой страны при открытии страницы
-showPhotos(0);
-
-
-
-const links = document.querySelectorAll('.choise a');
-
-links.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        links.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
-    });
-});
-
-
-
-
-// Плавный скролл к блоку при нажатии на ссылку
-document.addEventListener("DOMContentLoaded", function() {
-  const navLinks = document.querySelectorAll(".choise a");
-  
-  navLinks.forEach(link => {
-    link.addEventListener("click", scrollToSection);
-  });
-  
-  function scrollToSection(event) {
-    event.preventDefault();
-    
-    const targetId = event.currentTarget.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth"
-      });
+  arrowLeft.addEventListener('click', function () {
+    if (currentIndex > 0) {
+      currentIndex--
+      updateCarousel()
     }
+  })
+
+  arrowRight.addEventListener('click', function () {
+    if (currentIndex < carousel.children.length - 1) {
+      currentIndex++
+      updateCarousel()
+    }
+  })
+
+  function updateCarousel() {
+    const translateValue = -currentIndex * 130 // Ширина одной картинки
+    carousel.style.transform = `translateX(${translateValue}px)`
   }
-});
-
-
-
-jquery('.send-form').clic( function(){
-  var form = jquery(this).closest('form');
-
-  if ( form.valid()) {
-
-    var actUrl = form.attr('action');
-    jquery.ajax({
-      url: actUrl,
-      type: 'post',
-      dataType: 'html',
-      data: form.serialize(),
-      success:function(data) {
-
-
-        form.find('.status').html('form send');
-      },
-      error: function(){
-        form.find('.status').html('error');
-      }
-    });
-  }
-});
-
-
-
-
-
+})
